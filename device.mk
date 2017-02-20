d7261244075c9e7bcebe7da84b7188bf113bb58f
Kevin F. Haggerty <haggertk@lineageos.org>
2017-02-20 05:36:48 -0700

    Revert "klteduos: Import gps.conf file into device tree"
    This reverts commit 28339fe49976bc4c59baec1fa1e4f216cb7aa135.
    
    Change-Id: Ib632543272b566845806c61640992f5460d9c238

--- device.mk
+++ device.mk
@@ -27,10 +27,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PACKAGES += \
     init.target.rc
 
-# GPS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
-
 # NFC
 # See https://github.com/CyanogenMod/android_external_libnfc-nci/blob/cm-14.1/halimpl/pn54x/Android.mk#L21
 # for magic values of NXP_CHIP_TYPE.
