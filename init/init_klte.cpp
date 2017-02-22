f516efb85bbbf4c8149a14f57046ee79529e284f
Kevin F. Haggerty <haggertk@lineageos.org>
2017-02-22 07:14:33 -0700

    klteduos: libinit: Remove vestigal RIL properties
    * newDialCode is no longer a legacy RIL feature
    
    Change-Id: Ifd27a7a0a6606c8782312b5d711a3657660dbc0a

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2016, The Linux Foundation. All rights reserved.
+   Copyright (c) 2017, The LineageOS Project. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -41,7 +42,6 @@ void gsm_properties()
 {
     property_set("telephony.lteOnGsmDevice", "1");
     property_set("ro.telephony.default_network", "9");
-    property_set("ro.telephony.ril.config", "newDialCode");
 }
 
 void init_target_properties()
