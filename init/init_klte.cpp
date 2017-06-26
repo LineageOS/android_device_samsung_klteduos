5dab473c6c05f3df45a34c5875b39d95a7bf7e43
Bruno Martins <bgcngm@gmail.com>
2017-06-26 16:13:10 -0500

    klteduos: Abandon TARGET_UNIFIED_DEVICE flag
    * Override the unified properties using vendor init
    
    Change-Id: I71a63cb58cdf3e7c81f3df56e343a3047d6f21b0

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -54,17 +54,17 @@ void init_target_properties()
 
     if (bootloader.find("G900FD") == 0) {
         /* klteduosxx */
-        property_set("ro.build.fingerprint", "samsung/klteduosxx/klte:6.0.1/MMB29M/G900FDXXS1CQD2:user/release-keys");
-        property_set("ro.build.description", "klteduosxx-user 6.0.1 MMB29M G900FDXXS1CQD2 release-keys");
-        property_set("ro.product.model", "SM-G900FD");
-        property_set("ro.product.device", "klte");
+        property_override("ro.build.fingerprint", "samsung/klteduosxx/klte:6.0.1/MMB29M/G900FDXXS1CQD2:user/release-keys");
+        property_override("ro.build.description", "klteduosxx-user 6.0.1 MMB29M G900FDXXS1CQD2 release-keys");
+        property_override("ro.product.model", "SM-G900FD");
+        property_override("ro.product.device", "klte");
         gsm_properties();
     } else if (bootloader.find("G900MD") == 0) {
         /* klteduosub */
-        property_set("ro.build.fingerprint", "samsung/klteduosub/klte:6.0.1/MMB29M/G900MDUBS1CQD2:user/release-keys");
-        property_set("ro.build.description", "klteduosub-user 6.0.1 MMB29M G900MDUBS1CQD2 release-keys");
-        property_set("ro.product.model", "SM-G900MD");
-        property_set("ro.product.device", "klte");
+        property_override("ro.build.fingerprint", "samsung/klteduosub/klte:6.0.1/MMB29M/G900MDUBS1CQD2:user/release-keys");
+        property_override("ro.build.description", "klteduosub-user 6.0.1 MMB29M G900MDUBS1CQD2 release-keys");
+        property_override("ro.product.model", "SM-G900MD");
+        property_override("ro.product.device", "klte");
         gsm_properties();
     }
 
