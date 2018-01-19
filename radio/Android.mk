d68689225fbbcfaac0b390c7dbf2378ec5c8c910
Kevin F. Haggerty <haggertk@lineageos.org>
2018-01-19 14:14:54 -0700

    klteduos: Relocate/rename DSDS init script
    * There is no outstanding reason for this to be installed to the
      ramdisk, so let's just move it to vendor/etc/init to live
      alongside rild.rc.
    * Rename it to rild_dsds.rc for consistency with the primary rild
      init script.
    
    Change-Id: I6f7cdeeb5386412b41e049b574259f9e6aaea3b2

--- radio/Android.mk
+++ radio/Android.mk
@@ -16,12 +16,11 @@
 #
 
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.dsds.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.dsds.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE               := rild_dsds.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := rild_dsds.rc
+LOCAL_MODULE_RELATIVE_PATH := init
+LOCAL_VENDOR_MODULE        := true
 include $(BUILD_PREBUILT)
